22d34f7e5bc50ce845f1466753ddf6e506b2db71
acbewley <acbewley13@gmail.com>
2021-04-21 17:44:10 -0400

    got all pwa stuff working

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,65 +2,80 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/styles.css',
-    '/index.js',
+    '/dist/bundle.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
 ];
 
-const CACHE_NAME = 'static-cache-v1';
+const STATIC_CACHE = 'static-cache-v1';
 const DATA_CACHE_NAME = 'data-cache-v1';
+const RUNTIME_CACHE = 'runtime-cache';
 
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+        caches.open(STATIC_CACHE).then((cache) => cache.addAll(FILES_TO_CACHE))
     );
 
     self.skipWaiting();
 });
 
-self.addEventListener("activate", function (evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
+self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+      caches
+        .keys()
+        .then(cacheNames => {
+          return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
         })
-    );
-
-    self.clients.claim();
-});
-
-self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-  
-              return response;
+        .then(cachesToDelete => {
+          return Promise.all(
+            cachesToDelete.map(cacheToDelete => {
+              return caches.delete(cacheToDelete);
             })
-            .catch(err => {
-              return cache.match(evt.request);
-            });
-        }).catch(err => console.log(err))
-      );
-  
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+
+  self.addEventListener("fetch", event => {
+    if (
+      event.request.method !== "GET" ||
+      !event.request.url.startsWith(self.location.origin)
+    ) {
+      event.respondWith(fetch(event.request));
       return;
     }
   
-    evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
+    if (event.request.url.includes("/api/transaction")) {
+      event.respondWith(
+        caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              cache.put(event.request, response.clone());
+              return response;
+            })
+            .catch(() => caches.match(event.request));
+        })
+      );
+      return;
+    }
+  
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open(RUNTIME_CACHE).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
         });
       })
     );
   });
+  
--- public/webpack.config.js
+++ public/webpack.config.js
@@ -7,8 +7,29 @@ const config = {
         path: __dirname + '/dist',
         filename: 'bundle.js'
     },
-    mode: 'development',
-    plugins:
+    mode: 'production',
+    plugins: [
+        new WebpackPwaManifest ({
+            filename: 'manifest.json',
+            inject: false,
+            fingerprints: false,
+            name: 'Spending Tracker',
+            short_name: 'Spending Tracker',
+            theme_color: '#ffffff',
+            background_color: '#ffffff',
+            start_url: '/',
+            display: 'standalone',
+            icons: [
+                {
+                    src: path.resolve(
+                        __dirname,
+                        'icons/icon-512x512.png'
+                    ),
+                    size: [72, 96, 128, 144, 152, 192, 384, 512]
+                }
+            ]
+        })
+    ]
 };
 
 module.exports = config;
